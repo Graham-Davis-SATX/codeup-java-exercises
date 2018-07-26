b9416e0537943099e750dd6cb6e7581cb45c36b6
Graham Davis <davis.graham.satx@gmail.com>
2018-07-26 11:54:32 -0500

    adding user input validation - not complete

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,10 +1,11 @@
+import java.io.*;
 import java.util.Scanner;
 
 public class MethodsExercises {
 
     public static int addTwoInt(int a, int b) {
-           int sum = a + b;
-           return sum;
+        int sum = a + b;
+        return sum;
     }
 
     public static int subTwoInt(int a, int b) {
@@ -16,7 +17,7 @@ public class MethodsExercises {
         // int multi = a * b;
         // return multi;
         int result = 0;
-        for(int i=0;i<b;i++){
+        for (int i = 0; i < b; i++) {
             result = result + a;
         }
         return result;
@@ -26,7 +27,7 @@ public class MethodsExercises {
         // float div = a / b;
         // return div;
         int result = 0;
-        while(a >= b){
+        while (a >= b) {
             a = a - b;
             result = result + 1;
         }
@@ -40,15 +41,20 @@ public class MethodsExercises {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("We\'re going to perform mathematical operations on two numbers, but we need you to supply the two numbers.");
-        System.out.println("Please enter the first number you'd like to use: ");
-        int a = Integer.parseInt(sc.nextLine());
-        System.out.println("Please enter the second number you'd like to use: ");
-        int b = Integer.parseInt(sc.nextLine());
-        System.out.println("When you add " + "'" + a + "'" + " to " + "'" + b + "'" + " the result is: " + addTwoInt(a, b));
-        System.out.println("When you subtract " + "'" + b + "'" + " from " + "'" + a + "'" + " the result is: " + subTwoInt(a, b));
-        System.out.println("When you multiply " + "'" + a + "'" + " and " + "'" + b + "'" + " the result is: " + multiTwoInt(a, b));
-        System.out.println("When you divide " + "'" + a + "'" + " by " + "'" + b + "'" + " the result is: " + divTwoInt(a, b));
-        System.out.println("When you divide " + "'" + a + "'" + " by " + "'" + b + "'" + " you\'ll have a remainder of: " + modTwoInt(a, b));
-    }
+
+        BufferedReader reader=new BufferedReader(new InputStreamReader(System.in)); int no =0;
+
+//        System.out.println("We\'re going to perform mathematical operations on two numbers, but we need you to supply the two numbers.");
+//        System.out.println("Please enter the first number you'd like to use: ");
+//        int a = Integer.parseInt(sc.nextLine());
+//        System.out.println("Please enter the second number you'd like to use: ");
+//        int b = Integer.parseInt(sc.nextLine());
+//        System.out.println("When you add " + "'" + a + "'" + " to " + "'" + b + "'" + " the result is: " + addTwoInt(a, b));
+//        System.out.println("When you subtract " + "'" + b + "'" + " from " + "'" + a + "'" + " the result is: " + subTwoInt(a, b));
+//        System.out.println("When you multiply " + "'" + a + "'" + " and " + "'" + b + "'" + " the result is: " + multiTwoInt(a, b));
+//        System.out.println("When you divide " + "'" + a + "'" + " by " + "'" + b + "'" + " the result is: " + divTwoInt(a, b));
+//        System.out.println("When you divide " + "'" + a + "'" + " by " + "'" + b + "'" + " you\'ll have a remainder of: " + modTwoInt(a, b));
+
+    } // end main
+
 }

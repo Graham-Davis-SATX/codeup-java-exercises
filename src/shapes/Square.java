b67e7310d449ddd735fbc595a0c40848e19c361c
Graham Davis <davis.graham.satx@gmail.com>
2018-08-01 14:23:10 -0500

    uploading polymorph exercises

---
+++ src/shapes/Square.java
@@ -0,0 +1,17 @@
+package shapes;
+
+public class Square extends Rectangle {
+    public Square(double side) {
+        super(side, side);
+    }
+
+    @Override
+    public double getArea() {
+        return 4 * super.length;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return Math.pow(super.length, 2);
+    }
+}

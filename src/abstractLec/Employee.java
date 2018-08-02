32520a273d6ad45ad4f930589d4f044008f177a2
Graham Davis <davis.graham.satx@gmail.com>
2018-08-02 13:44:41 -0500

    adding abstract exercise

---
+++ src/abstractLec/Employee.java
@@ -0,0 +1,6 @@
+package abstractLec;
+
+abstract class Employee {
+    public abstract String work();
+    public abstract String quit();
+}
--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,20 +1,47 @@
 package shapes;
 
-public class Square extends Rectangle {
+//public class Square extends Rectangle {
+//
+//    public Square(double side) {
+//        super(side, side);
+//    }
+//
+//    @Override
+//    public double getArea() {
+//        System.out.println("overWrote");
+//        return 4 * super.length;
+//    }
+//
+//    @Override
+//    public double getPerimeter() {
+//        System.out.println("overWrote");
+//        return Math.pow(super.length, 2);
+//    }
+//}
+
+public class Square extends Quadrilateral{
 
     public Square(double side) {
         super(side, side);
     }
 
     @Override
-    public double getArea() {
-        System.out.println("overWrote");
-        return 4 * super.length;
+    public void setLength(double side) {
+        super.length = side;
+    }
+
+    @Override
+    public void setWidth(double side) {
+        super.width = side;
     }
 
     @Override
     public double getPerimeter() {
-        System.out.println("overWrote");
-        return Math.pow(super.length, 2);
+        return super.length * 4;
+    }
+
+    @Override
+    public double getArea() {
+        return Math.pow(super.width, 2);
     }
 }

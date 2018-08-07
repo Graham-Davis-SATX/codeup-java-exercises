cdde2da5655c71361f4b907c045bf8e4835d1ebe
Graham Davis <davis.graham.satx@gmail.com>
2018-08-07 13:40:07 -0500

    adding basic IO lec

--- src/BasicFileIODir/BasicFileIO.java
+++ src/BasicFileIODir/BasicFileIO.java
@@ -1,45 +1,156 @@
-package BasicFileIODir;
-
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Scanner;
-
+import java.util.List;
 
 public class BasicFileIO {
 
     public static void main(String[] args) {
 
-//        // BasicFileIo Path
-//        Path BasicFileIOPath = Paths.get("src/BasicFileIODir", "BasicFileIO.java");
-//
-//        System.out.println(BasicFileIOPath);
-//
-        File file = new File("../codeup-java-exercises/src/BasicFileIODir/test.txt");
-//
-//        //Write your name and age to the file
+    Path BasicFileIOPath = Paths.get("src", "BasicFileIODir/BasicFileIO.java");
+
+//    ============================ PATH
+
+
+    System.out.println(BasicFileIOPath);
+    System.out.println(BasicFileIOPath.toAbsolutePath());
+//    Path otherPath = Paths.get("src", "../", "src", "../", "src", "FileIOLecture");
+//    System.out.println(otherPath);
+//    System.out.println(otherPath.normalize());
+//    System.out.println(otherPath.normalize().toAbsolutePath());
+//    System.out.println(otherPath.toAbsolutePath());
+
+//    ============================ FILE METHODS
+
+            System.out.println(Files.exists(BasicFileIOPath));
+            System.out.println(Files.exists(Paths.get("src", "BasicFileIODir/Data/info.txt")));
+
+            String directory = "data";
+            String filename = "info.txt";
+            Path dataDirectory = Paths.get(directory);
+            Path dataFile = Paths.get(directory, filename);
+
+            if (!Files.isDirectory(dataDirectory)) {
+                try {
+                    Files.createDirectories(dataDirectory);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            if (!Files.exists(dataFile)) {
+                try {
+                    Files.createFile(dataFile);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+
+//    ============================ READING FILES
+
 //        try {
-//            PrintWriter output = new PrintWriter(file);
-//            output.println("Graham Davis");
-//            output.println(30);
-//            output.close();
-//        } catch (IOException ex) {
-//            System.out.printf("ERROR: %s\n", ex);
+//            List<String> contents = Files.readAllLines(
+//                    Paths.get("codeup-java-exercises", "src/data/info.txt")
+//            );
+//        } catch (Exception e) {
+//            System.out.println("Exception!");
+//            e.printStackTrace();
+//        }
+//
+//            try {
+//                List<String> contents = Files.readAllLines(Paths.get("data/info.txt"));
+//                System.out.println(contents.toString());
+//            } catch (IOException e) {
+//                System.out.println("Exception!");
+//                e.printStackTrace();
+//            }
+
+
+        // ============================ WRITING FILES
+
+//            Path p = Paths.get("./src/lec",  "test.txt");
+//
+//            List<String> names = new ArrayList<>();
+//            names.add("Tim");
+//            names.add("Sally");
+//            names.add("Mary");
+//            try {
+//                Files.write(p, names, StandardOpenOption.APPEND);
+//            } catch(IOException e) {
+//                e.printStackTrace();
+//            }
+//
+//            String directory = "data";
+//            String filename = "info.txt";
+//            Path dataDirectory = Paths.get(directory);
+//            Path dataFile = Paths.get(directory, filename);
+//
+//            List<String> nums = new ArrayList<>();
+//            nums.add("1");
+//            nums.add("2");
+//            nums.add("3");
+//
+//            try {
+//                Files.write(dataFile, nums, StandardOpenOption.APPEND);
+//            } catch(IOException e) {
+//                System.out.println("Exception!!!");
+//                e.printStackTrace();
+//            }
+
+        // ============================ ADDITIONAL SLIDE EXAMPLES
+
+
+        // Print out each line in an existing file, along with the line number
+
+//        Path groceriesPath = Paths.get("data", "groceries.txt");
+//        List<String> groceryList = null;
+//        try {
+//            groceryList = Files.readAllLines(groceriesPath);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//        for (int i = 0; i < groceryList.size(); i += 1) {
+//            System.out.println((i + 1) + ": " + groceryList.get(i));
 //        }
 
-        //Reads from the file
-        try {
-            Scanner input = new Scanner(file);
-            String name = input.nextLine();
-            int age = input.nextInt();
 
-            System.out.printf("Name: %s\nAge: %d\n", name, age);
-        } catch (FileNotFoundException ex) {
-            System.out.printf("ERROR: %s\n", ex);
-        }
+        // Add a line to an existing file
+
+//        try {
+//            Files.write(
+//                Paths.get("data", "groceries.txt"),
+//                Arrays.asList("eggs"), // list with one item
+//                StandardOpenOption.APPEND
+//            );
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+        // Replace "milk" with "cream"
+
+//        List<String> lines = null;
+//        try {
+//            lines = Files.readAllLines(Paths.get("data", "groceries.txt"));
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        List<String> newList = new ArrayList<>();
+//        for (String line : lines) {
+//            if (line.equals("milk")) {
+//                newList.add("cream");
+//                continue;
+//            }
+//            newList.add(line);
+//        }
+//
+//        try {
+//            Files.write(Paths.get("data", "groceries.txt"), newList);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
     }
-
 }
